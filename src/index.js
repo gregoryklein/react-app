ff38af1feb0f7094d12756b8670ff6e9d9d0290a
Greg Klein <greg-klein@outlook.com>
2019-01-15 08:04:51 -0700

    more mosh

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World</h1>;
-console.log(element);
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
