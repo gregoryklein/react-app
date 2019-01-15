2c3c872f4a8f64d2126069924af1278aadc54946
Greg Klein <greg-klein@outlook.com>
2019-01-15 13:49:08 -0700

    55:06 mosh

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,20 +3,29 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    imageUrl: "https://picsum.photos/200"
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <div>
         {/*<img src={this.state.imageUrl} alt="poop" />*/}
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-danger btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
